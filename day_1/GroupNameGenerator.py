3a7c3c45b3bc823bab8332e0685d7957a51f57ff
Marco Aceves <85464208+marcoaceves@users.noreply.github.com>
2022-09-04 15:47:15 -0700

    clean up code

--- day_1/GroupNameGenerator.py
+++ day_1/GroupNameGenerator.py
@@ -1,8 +1,11 @@
 # Group Name Generator 
 
+print('Welcome to Group Name Generator ')
 
-name1 = input("Enter your favorite word: ")
-name2 = input("Enter Your Favorite Animal: ")
+
+name1 = input("Enter your favorite word:\n ")
+name2 = input("Enter Your Favorite Animal:\n ")
 Groupname= name1 +" " +name2 + "'s"
-print(Groupname)
+
+print("Your Group Name is: ", Groupname)
 
